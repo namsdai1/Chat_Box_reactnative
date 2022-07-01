9e99a2779d5bbb8942895a383c0a4c04937822d2
Thanh Nam <namsdai12@gmail.com>
2022-07-01 13:37:52 +0700

    Chat

--- src/data/Chats.js
+++ src/data/Chats.js
@@ -17,6 +17,7 @@ export default {
   messages: [
     {
       id: 'm1',
+      type: 'text',
       content: 'How are you, Elon!',
       createdAt: '2020-10-02T12:48:00.000Z',
       user: {
@@ -26,6 +27,7 @@ export default {
     },
     {
       id: 'm2',
+      type: 'text',
       content: 'I am good, good',
       createdAt: '2020-10-03T14:49:00.000Z',
       user: {
@@ -35,6 +37,7 @@ export default {
     },
     {
       id: 'm3',
+      type: 'text',
       content: 'What about you?',
       createdAt: '2020-10-04T14:49:40.000Z',
       user: {
@@ -44,6 +47,7 @@ export default {
     },
     {
       id: 'm4',
+      type: 'text',
       content: 'Good as well, preparing for the stream now.',
       createdAt: '2020-10-07T14:47:00.000Z',
       user: {
@@ -53,6 +57,7 @@ export default {
     },
     {
       id: 'm5',
+      type: 'text',
       content: 'How is SpaceX doing?',
       createdAt: '2020-10-07T14:48:00.000Z',
       user: {
@@ -62,6 +67,7 @@ export default {
     },
     {
       id: 'm6',
+      type: 'text',
       content: 'going to the Moooooon',
       createdAt: '2020-10-07T14:49:00.000Z',
       user: {
@@ -71,6 +77,7 @@ export default {
     },
     {
       id: 'm6',
+      type: 'text',
       content: 'going to the Moooooon',
       createdAt: '2020-10-07T14:49:00.000Z',
       user: {
@@ -80,6 +87,7 @@ export default {
     },
     {
       id: 'm6',
+      type: 'text',
       content: 'going to the Moooooon',
       createdAt: '2020-10-07T14:49:00.000Z',
       user: {
@@ -89,6 +97,7 @@ export default {
     },
     {
       id: 'm7',
+      type: 'text',
       content: 'btw, SpaceX is interested in buying notJust.dev!',
       createdAt: 1656497877769,
       user: {
@@ -98,6 +107,17 @@ export default {
     },
     {
       id: 'm7',
+      type: 'text',
+      content: 'btw, SpaceX is interested in buying notJust.dev!',
+      createdAt: 1656497877769,
+      user: {
+        id: 'u2',
+        name: 'Elon Musk',
+      },
+    },
+    {
+      id: 'm7',
+      type: 'image',
       content: 'btw, SpaceX is interested in buying notJust.dev!',
       createdAt: 1656497877769,
       user: {
--- src/screens/chatbox/ChatBox.js
+++ src/screens/chatbox/ChatBox.js
@@ -48,7 +48,27 @@ function ChatBox() {
   useEffect(() => {
     console.log('Messages cap nhat');
   }, [messages]);
+  function isEmoji(str) {
+    console.log(str, 'aa');
+    var regex =
+      /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+    if (str.match(regex)) {
+      const check = str.replace(regex, '');
+      console.log('check', check, 'check');
+      if (check == '') return true;
+      else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  function removeEmojis(string) {
+    var regex =
+      /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
 
+    return string.replace(regex, '');
+  }
   return (
     <>
       <View style={{backgroundColor: '#f1f1f1', height: '100%'}}>
@@ -62,7 +82,8 @@ function ChatBox() {
             var indexPrev = 0;
             var index2 = 0;
             var dateTime = '';
-
+            const checkEmoji = isEmoji(value.content);
+            console.log(checkEmoji, 'isEmoji');
             if (index !== messages.length - 1) {
               index2 = index + 1;
             }
@@ -155,13 +176,17 @@ function ChatBox() {
                       <View
                         style={{
                           marginHorizontal: 10,
-                          backgroundColor: 'white',
+                          backgroundColor: checkEmoji ? null : 'white',
                           paddingHorizontal: 10,
                           paddingVertical: 8,
                           borderRadius: 10,
                           backgroundColor: 'pink',
                         }}>
-                        <Text style={{color: 'gray', fontSize: 16}}>
+                        <Text
+                          style={{
+                            color: 'gray',
+                            fontSize: checkEmoji ? 24 : 16,
+                          }}>
                           {value.content}
                         </Text>
                       </View>
@@ -207,12 +232,16 @@ function ChatBox() {
                       <View
                         style={{
                           marginHorizontal: 10,
-                          backgroundColor: 'white',
+                          backgroundColor: checkEmoji ? null : 'white',
                           paddingHorizontal: 10,
-                          paddingVertical: 8,
+                          paddingVertical: checkEmoji ? 2 : 8,
                           borderRadius: 10,
                         }}>
-                        <Text style={{color: 'gray', fontSize: 16}}>
+                        <Text
+                          style={{
+                            color: 'gray',
+                            fontSize: checkEmoji ? 24 : 16,
+                          }}>
                           {value.content}
                         </Text>
                       </View>
@@ -273,14 +302,14 @@ const BottomChat = ({keyBoard, setMessages, messages}) => {
   const textInputRef = useRef(null);
   const [fastEmoji, setFastEmoji] = useState('🖤');
   const emojiRef = useRef(new Animated.Value(0)).current;
+  const viewEdtRef = useRef(new Animated.Value(0)).current;
   useEffect(() => {
-    if (showEmoji && !keyBoard) moveAnimation();
+    if (showEmoji && !keyBoard) null;
     else if (keyBoard) {
       setShowEmoji(false);
-      moveAnimation();
-    } else {
-      moveAnimation();
     }
+    moveAnimation();
+    moveTextInput();
   }, [showEmoji, keyBoard]);
   // useEffect(() => {
   //   first
@@ -289,7 +318,14 @@ const BottomChat = ({keyBoard, setMessages, messages}) => {
   //     second
   //   }
   // }, [third])
-
+  const moveTextInput = () => {
+    console.log('moveText');
+    Animated.timing(viewEdtRef, {
+      toValue: !(keyBoard || showEmoji) ? width * 0.52 : width * 0.76,
+      duration: 200,
+      useNativeDriver: false,
+    }).start();
+  };
   const moveAnimation = () => {
     console.log('aa');
     Animated.timing(emojiRef, {
@@ -406,37 +442,51 @@ const BottomChat = ({keyBoard, setMessages, messages}) => {
           borderWidth: 0.1,
           borderTopColor: 'back',
           flexDirection: 'row',
-          justifyContent: 'space-around',
+          justifyContent: 'flex-end',
           alignItems: 'center',
         }}>
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <View
+          style={{
+            width: '12%',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
           <AntDesign name="like1" color={'black'} size={22} />
         </View>
-        {!keyBoard && (
+        {!(keyBoard || showEmoji) && (
           <TouchableOpacity
             onPress={() => {
               launchCameraRN();
             }}
-            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            style={{
+              width: '12%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
             <AntDesign name="like1" color={'black'} size={22} />
           </TouchableOpacity>
         )}
 
-        {!keyBoard && (
+        {!(keyBoard || showEmoji) && (
           <TouchableOpacity
             onPress={() => {
               launchImageLibraryRN();
             }}
-            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            style={{
+              width: '12%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
             <AntDesign name="like1" color={'black'} size={22} />
           </TouchableOpacity>
         )}
-        <View
+        <Animated.View
           style={{
-            flex: 4,
+            // flex: viewEdtRef,
+            width: viewEdtRef,
             paddingHorizontal: 4,
             backgroundColor: '#e0e0d1',
-            width: '50%',
+
             borderRadius: 20,
           }}>
           <View
@@ -466,12 +516,11 @@ const BottomChat = ({keyBoard, setMessages, messages}) => {
               <FontAwesome5 name="smile" color={'black'} size={22} />
             </TouchableOpacity>
           </View>
-        </View>
+        </Animated.View>
         <Pressable
           onPress={_setMessages}
           style={{
-            flex: 1,
-
+            width: '12%',
             justifyContent: 'center',
             alignItems: 'center',
           }}>
